84380a3bc0d91b0efa1265a737e167248f3b8090
Urviskumar <urvishp80@gmail.com>
2024-08-26 11:01:52 -0400

    Update the API input format to use JSON.

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,4 +1,5 @@
 import logging
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, HTTPException, Form, Depends, Request
 from services.chatbot_service import get_openai_response_with_langchain, clear_chat_history, get_db
@@ -9,10 +10,20 @@ router = APIRouter(
 )
 
 
+# Parsing JSON input
+class AskQuestionRequest(BaseModel):
+    user_question: str
+    user_id: str
+
+
+class ClearChatRequest(BaseModel):
+    user_id: str
+
+
 @router.post("/ask")
-def ask_question(request: Request, user_question: str = Form(), user_id: str = Form(), db: Session = Depends(get_db)):
+async def ask_question(request: AskQuestionRequest, db: Session = Depends(get_db)):
     try:
-        return get_openai_response_with_langchain(user_question=user_question.strip(), db=db, user_id=user_id)
+        return get_openai_response_with_langchain(user_question=request.user_question.strip(), db=db, user_id=request.user_id)
     except Exception as e:
         logging.error(f"Error in /ask endpoint: {str(e)}")
         raise HTTPException(
@@ -22,9 +33,9 @@ def ask_question(request: Request, user_question: str = Form(), user_id: str = F
 
 
 @router.delete("/clear-chat")
-def clear_chat(user_id: str = Form(), db: Session = Depends(get_db)):
+async def clear_chat(request: ClearChatRequest, db: Session = Depends(get_db)):
     try:
-        return clear_chat_history(user_id=user_id, db=db)
+        return clear_chat_history(user_id=request.user_id, db=db)
     except Exception as e:
         logging.error(f"Error in /clear-chat endpoint: {str(e)}")
         raise HTTPException(
