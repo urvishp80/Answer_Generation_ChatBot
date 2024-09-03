c98a62c85bfc7b268dfbcce74f25ef0b30f5b0f7
Urviskumar <urvishp80@gmail.com>
2024-09-03 13:09:38 -0400

    Update clear-chat API

--- api/endpoints.py
+++ api/endpoints.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, HTTPException, Form, Depends, Request
 from services.chatbot_service import get_openai_response_with_langchain, clear_chat_history, get_db
+from core.bot_history_db import ChatHistory
 
 router = APIRouter(
     prefix="/chatbots",
@@ -35,9 +36,19 @@ async def ask_question(request: AskQuestionRequest, db: Session = Depends(get_db
 @router.delete("/clear-chat")
 async def clear_chat(request: ClearChatRequest, db: Session = Depends(get_db)):
     try:
+        # Check if the user_id exists in the database
+        user_exists = db.query(ChatHistory).filter(ChatHistory.user_id == request.user_id).first() is not None
+        if not user_exists:
+            raise HTTPException(
+                status_code=404,
+                detail=f"No records found for user ID: {request.user_id}."
+            )
+
         return clear_chat_history(user_id=request.user_id, db=db)
+    except HTTPException as e:
+        raise e
     except Exception as e:
-        logging.error(f"Error in /clear-chat endpoint: {str(e)}")
+        logging.warning(f"Error in /clear-chat endpoint: {str(e)}")
         raise HTTPException(
             status_code=500,
             detail="Internal Server Error, please check the logs."
