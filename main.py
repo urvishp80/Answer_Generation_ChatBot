65f76949f0776331d511c5b18896e2673540d51f
Urviskumar <urvishp80@gmail.com>
2024-10-07 13:55:56 -0400

    Update port in main.py

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ app = FastAPI()
 app.include_router(chatbot_router)
 
 @app.get("/")
-# def read_root():
 async def read_root():
     return {"message": "Welcome to the Clearbuy Product Recommendation Chatbot API"}
 
 if __name__ == '__main__':
     import uvicorn
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 5000))
     uvicorn.run(app, host="0.0.0.0", port=port)
